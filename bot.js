56fda508b7c9af85e0ae5973825854b361345732
AlkyoneAoide <playinggameswithfriends@gmail.com>
2020-05-05 20:56:46 -0600

    added jsdom and main function

--- bot.js
+++ bot.js
@@ -1,14 +1,23 @@
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var json = require("./token.json");
+const json = require("./token.json");
 const botToken = json["token"];
 
-client.once('ready', () => {
+function main() {
+	client.once('ready', () => {
 	console.log('Ready!');
-});
+	});
+
+	client.login(botToken);
+}
 
 client.on('message', message => {
 	console.log(message.content);
 });
 
-client.login(botToken);
+document.addEventListener("DOMContentLoaded", main());
