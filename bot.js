ac749b3a3ca66c3a72fca965c79593b6141cb6a9
AlkyoneAoide <playinggameswithfriends@gmail.com>
2020-05-06 17:53:56 -0600

    fixed command detection

--- bot.js
+++ bot.js
@@ -21,18 +21,31 @@ function main() {
 }
 
 
-//Put all bot code here
+//What happens every time a message is picked up by the bot
 client.on('message', message => {
 	//Check if message starts with the prefix (default '\')
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	//Take everything after the prefix and split it into an array
 	//For example, \help roll will become [help,roll]
-	let args = message.content.slice(prefix.length).split(' ');
-	let command = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).split(/ +/).toLowerCase();
+
+	//If args.length is true (>0) do runCommand(), else do help()
+	args.length ? runCommand() : help()
+
+//Command Programming
+function runCommand() {
+	return message.channel.send(`You wanted to run ${args}`)
+}
+
+//What to do if no command was specified
+function help() {
+	return message.channel.send("Unspecified Command");
+}
+
 });
 
 
-//====================================================
-// Once the document is loaded, run the main function
-//====================================================
+//===========================================================
+// Once the entire document is loaded, run the main function
+//===========================================================
 document.addEventListener("DOMContentLoaded", main());
