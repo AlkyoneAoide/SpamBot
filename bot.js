2ef798600ebef6d99273b3dee849f8fa138aff2b
AlkyoneAoide <playinggameswithfriends@gmail.com>
2020-05-05 18:44:04 -0600

    token

--- bot.js
+++ bot.js
@@ -9,6 +9,6 @@ client.on('message', message => {
 	console.log(message.content);
 });
 
-client.login('');
+client.login('NzA3MzgzNTc3NTg2Njk2Mjk0.XrIAdw._M2R8DRc6tEExaZiPCryAjv2iGo');
 
 client.user.setStatus('invisible');
